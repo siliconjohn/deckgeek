cc76ef85074b52696b5cfbc2060cb1e5a91fa965
john doerfler <johndoerfler@gmail.com>
2013-05-21 14:14:42 -0500

    changed relations in model

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,5 +1,5 @@
 class Card < ActiveRecord::Base
   belongs_to :deck, :inverse_of => :cards
-  has_one :theme;
+  belongs_to :theme, :inverse_of => :cards
   attr_accessible :description, :name, :deck_id, :theme_id
 end

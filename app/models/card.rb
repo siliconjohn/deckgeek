83c4d6419e2cd68957ddfbdb34e43b9b26219d97
john doerfler <johndoerfler@gmail.com>
2013-05-28 23:12:54 -0500

    Added db relations

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,6 +1,7 @@
 class Card < ActiveRecord::Base
   belongs_to :deck, :inverse_of => :cards
   belongs_to :style, :inverse_of => :cards
+  belongs_to :image, :inverse_of => :cards
 
   attr_accessible :description, :name, :deck_id, :style_id
 
--- app/models/image.rb
+++ app/models/image.rb
@@ -1,4 +1,5 @@
 class Image < ActiveRecord::Base
+  has_many :cards;
   attr_accessible :name, :url
   validates_length_of :name, :maximum => 255
 end

f52cca2354707248995f9b576b4846d045d5a56f
siliconjohn <johndoerfler@gmail.com>
2013-06-29 20:16:11 -0500

    updates tests

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -2,10 +2,10 @@ require 'spec_helper'
 
 describe GamesController do
 
-  describe "GET index" do
-  it "response is 200" do
-       get :index
-     expect(response.status).to eq(200)
-  end
-end
+#   describe "GET index" do
+#   it "response is 200" do
+#        get :index
+#      expect(response.status).to eq(200)
+#   end
+# end
 end

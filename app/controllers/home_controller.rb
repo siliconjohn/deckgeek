7e6c3e4547b66887d500500e291e8a97731450ca
siliconjohn <johndoerfler@gmail.com>
2013-07-10 17:14:33 -0500

    added redirect to sign in when visiting root url

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,7 @@ class HomeController < ApplicationController
     if current_user
       render "home"
     else
-      #fix render :controller => "Devise::SessionsController", :action => "new"
+      redirect_to new_user_session_path
     end
   end
 end

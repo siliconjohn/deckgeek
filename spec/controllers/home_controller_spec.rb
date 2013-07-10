91f6b31ee19763eec83a144f0fe39331e3a8e3a9
siliconjohn <johndoerfler@gmail.com>
2013-07-10 17:23:34 -0500

    updated test

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -5,9 +5,8 @@ describe HomeController do
   describe "Not Logged in" do
     it "render non logged in home page" do
       get :index
-      expect(response.status).to eq(200)
+      expect(response.status).to eq(302)
       response.header[ 'Content-Type' ].should include 'text/html'
-      response.should render_template( "home/index" )
     end
   end
 

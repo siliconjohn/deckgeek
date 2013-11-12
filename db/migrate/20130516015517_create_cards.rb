0f441f50b9cd2d7029c6d47eddedc965113be7be
John Doerfler <johndoerfler@gmail.com>
2013-11-12 12:05:54 -0800

    change var type of card html

--- db/migrate/20130516015517_create_cards.rb
+++ db/migrate/20130516015517_create_cards.rb
@@ -3,7 +3,7 @@ class CreateCards < ActiveRecord::Migration
     create_table :cards do |t|
       t.string :name, :default => "My Card"
       t.references :deck
-      t.string :html, :default => '<div class="jcard"><img class="jcard-bg-image" style="position: relative;" src="http://www.siliconjohn.com/deckgeek/images/2.jpg" alt="2.jpg" data-id="2"><div class="jcard-border"></div><div class="jcard-text" style="left: 17px; top: 16px;"><span>Card title, click me to change the text.</span></div><div class="jcard-text" style="left: 17px; top: 279px;"><span>Card description </span></div></div>'
+      t.text :html, :default => '<div class="jcard"><img class="jcard-bg-image" style="position: relative;" src="http://www.siliconjohn.com/deckgeek/images/2.jpg" alt="2.jpg" data-id="2"><div class="jcard-border"></div><div class="jcard-text" style="left: 17px; top: 16px;"><span>Card title, click me to change the text.</span></div><div class="jcard-text" style="left: 17px; top: 279px;"><span>Card description </span></div></div>'
       t.timestamps
     end
     add_index :cards, :deck_id

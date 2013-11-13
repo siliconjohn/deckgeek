a8ef625bd8ef86a9a499f24e2065676af50dfb8d
John Doerfler <johndoerfler@gmail.com>
2013-11-12 17:59:05 -0800

    c

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,11 +6,7 @@ class HomeController < ApplicationController
   def index
     if get_current_or_guest_user
    
-      if get_current_or_guest_user.admin
-       @game = Game.where( get_current_or_guest_user.id )
-      else
-       @game = Game.where( get_current_or_guest_user.id, :conditions => { :user_id => get_current_or_guest_user.id })
-      end
+       @game = Game.where(  :user_id => get_current_or_guest_user.id )
    
       render "home"
     else
@@ -21,7 +17,7 @@ class HomeController < ApplicationController
   def guest
   	if get_current_user_or_create_guest_user
       
-       @game = Game.where( get_current_or_guest_user.id, :conditions => { :user_id => get_current_or_guest_user.id })
+       @game = Game.where(  :user_id => get_current_or_guest_user.id )
        render "home"
     else
     	render "index"  

b0f3b0b71c02c3a4b76270b65ea5697ef2b31666
John Doerfler <johndoerfler@gmail.com>
2013-11-14 20:59:41 -0800

    se

--- config/environment.rb
+++ config/environment.rb
@@ -6,7 +6,7 @@ ActionMailer::Base.smtp_settings = {
   :port           => '587',
   :authentication => :plain,
   :user_name      => ENV['SENDGRID_USERNAME'],
-  :password       =>  ENV['SENDGRID_PASSWORD',
+  :password       => ENV['SENDGRID_PASSWORD'],
   :domain         => 'heroku.com',
   :enable_starttls_auto => true
 }

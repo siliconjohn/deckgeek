e2751ff273b8958b3a7a416fd791b75c32d39e41
John Doerfler <johndoerfler@gmail.com>
2013-11-14 20:57:36 -0800

    fixes unam and pass

--- config/environment.rb
+++ config/environment.rb
@@ -5,8 +5,8 @@ ActionMailer::Base.smtp_settings = {
   :address        => 'smtp.sendgrid.net',
   :port           => '587',
   :authentication => :plain,
-  :user_name      => ENV['app18557776@heroku.com'],
-  :password       => ENV['bvknjquu'],
+  :user_name      => ENV['SENDGRID_USERNAME'],
+  :password       =>  ENV['SENDGRID_PASSWORD',
   :domain         => 'heroku.com',
   :enable_starttls_auto => true
 }

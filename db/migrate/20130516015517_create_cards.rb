0c4454124c0110bff1b6b46d02c5bfd94553be9a
John Doerfler <johndoerfler@gmail.com>
2015-05-14 13:39:41 +0700

    changed location of images

--- db/migrate/20130516015517_create_cards.rb
+++ db/migrate/20130516015517_create_cards.rb
@@ -3,7 +3,7 @@ class CreateCards < ActiveRecord::Migration
     create_table :cards do |t|
       t.string :name, :default => "My Card"
       t.references :deck
-      t.text :html, :default => '<div class="jcard"><img class="jcard-bg-image" style="position: relative; left: -18px; top: -21px; display: inline-block; width: 328px;" src="http://www.siliconjohn.com/deckgeek/images/31.jpg" alt="31.jpg" data-id="31"><div class="jcard-border"></div><div class="jcard-text" style="left: 17px; top: 16px;"><div class="jtext">Click me to change this text.</div></div></div>'
+      t.text :html, :default => '<div class="jcard"><img class="jcard-bg-image" style="position: relative; left: -18px; top: -21px; display: inline-block; width: 328px;" src="http://deckgeek.herokuapp.com/assets/images/31.jpg" alt="31.jpg" data-id="31"><div class="jcard-border"></div><div class="jcard-text" style="left: 17px; top: 16px;"><div class="jtext">Click me to change this text.</div></div></div>'
       t.timestamps
     end
     add_index :cards, :deck_id
